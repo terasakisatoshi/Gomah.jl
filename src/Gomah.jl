bd4111b0706659108d3c45d7d4864272bca46582
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2019-05-05 07:55:38 +0900

    goma

--- src/Gomah.jl
+++ src/Gomah.jl
@@ -1,6 +1,20 @@
 module Gomah
-export chainer
-export train, test
-include("mnist.jl")
+
+using PyCall
+
+export chainer, train, predict
+
+
+function train()
+    chainer = pyimport("chainer")
+    mnist = chainer.datasets.mnist
+    mnist.get_mnist()
+end
+
+
+function predict()
+    nothing
+end
+#include("mnist.jl")
 
 end # module
