108365e90cbabb45887df842fa9b201b2ddc57c2
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2019-05-05 14:24:41 +0900

    progress Gomah Package

--- src/Gomah.jl
+++ src/Gomah.jl
@@ -2,19 +2,58 @@ module Gomah
 
 using PyCall
 
-export chainer, train, predict
+export chainer
+# defined in mnist.jl
+export train, predict
+export debug, get_model
+
+# chainer object
+const chainer = PyNULL()
+const math = PyNULL()
+const np = PyNULL()
+const Chain = PyNULL()
+const F = PyNULL()
+const L = PyNULL()
+const SerialIterator = PyNULL()
+const training = PyNULL()
+const extensions = PyNULL()
+const optimizers = PyNULL()
 
 
-function train()
-    chainer = pyimport("chainer")
-    mnist = chainer.datasets.mnist
-    mnist.get_mnist()
+# module initializer
+function __init__()
+    copy!(chainer, pyimport("chainer"))
+    copy!(math, pyimport("math"))
+    copy!(np, pyimport("numpy"))
+    copy!(Chain, chainer.Chain)
+    copy!(F, chainer.functions)
+    copy!(L, chainer.links)
+    copy!(SerialIterator, chainer.iterators.SerialIterator)
+    copy!(training, chainer.training)
+    copy!(extensions, training.extensions)
+    copy!(optimizers, chainer.optimizers)
 end
 
+#=
+ initialize pyobject to avoid error when define Python Class 
+using @pydef mutable struct ...
+=# 
 
-function predict()
-    nothing
+function debug()
+    batchsize = 128
+    epochs = 10
+    train_set, _ = get_mnist()
+    N=pybuiltin(:len)(train_set)
+    train_set, val_set = chainer.datasets.split_dataset(
+            train_set,
+            convert(Int, 0.9*N),
+            Random.shuffle(collect(0:N-1))
+        )
+    train_iter = SerialIterator(train_set, batchsize)
+    test_iter = SerialIterator(val_set, batchsize, repeat=false, shuffle=false)
+    model = get_model()
 end
-#include("mnist.jl")
+
+include("mnist.jl")
 
 end # module
