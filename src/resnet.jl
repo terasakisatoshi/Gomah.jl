86c6f18a0c8100f1687f95e60707793d3dbe810c
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2019-08-26 04:39:12 +0900

    format

--- src/resnet.jl
+++ src/resnet.jl
@@ -24,9 +24,9 @@ struct BottleNeckA
     layers
     residual_conv
     function BottleNeckA(link::PyObject)
-        layers=Conv2DBNActiv.([link.conv1,link.conv2,link.conv3])
+        layers = Conv2DBNActiv.([link.conv1, link.conv2, link.conv3])
         residual_conv = Conv2DBNActiv(link.residual_conv)
-        new(Chain(layers...),residual_conv)
+        new(Chain(layers...), residual_conv)
     end
 end
 
@@ -35,7 +35,7 @@ Flux.@treelike BottleNeckA
 function (bottleneck::BottleNeckA)(x)
     h1 = bottleneck.layers(x)
     h2 = bottleneck.residual_conv(x)
-    y =  Flux.relu.(h1 .+ h2)
+    y = Flux.relu.(h1 .+ h2)
     return y
 end
 
@@ -43,7 +43,7 @@ end
 struct BottleNeckB
     chain
     function BottleNeckB(link::PyObject)
-        layers=[link.conv1,link.conv2,link.conv3]
+        layers = [link.conv1, link.conv2, link.conv3]
         new(Chain(Conv2DBNActiv.(layers)...))
     end
 end
@@ -51,7 +51,7 @@ end
 Flux.@treelike BottleNeckB
 
 function (bottleneckB::BottleNeckB)(x)
-    h=bottleneckB.chain(x)
+    h = bottleneckB.chain(x)
     Flux.relu.(h .+ x)
 end
 
@@ -59,7 +59,7 @@ function ResBlock(link)
     layers = Any[]
     push!(layers, BottleNeckA(link.a))
     for name in link.layer_names[2:end]
-        chlay=py"getattr"(link, name)
+        chlay = py"getattr"(link, name)
         push!(layers, BottleNeckB(chlay))
     end
     Chain(layers...)
@@ -69,32 +69,32 @@ struct MyRes
     layers
 end
 
-num=50
+num = 50
 const PyResNet = chainercv.links.model.resnet.ResNet
 const resnet = PyResNet(num, pretrained_model = "imagenet")
 
 function myres()
-    dummyX=np.ones((1,3,224,224),dtype=np.float32)
+    dummyX = np.ones((1, 3, 224, 224), dtype = np.float32)
     resnet(dummyX)
     #resnet101=ResNet(101, pretrained_model="imagenet")
     #resnet152=ResNet(152, pretrained_model="imagenet")
     @show resnet.layer_names
     Chain(
         Conv2DBNActiv(resnet.conv1),
-        MaxPool((3,3), pad = (1,1), stride = (2,2)),
+        MaxPool((3, 3), pad = (1, 1), stride = (2, 2)),
         ResBlock(resnet.res2),
         ResBlock(resnet.res3),
         ResBlock(resnet.res4),
         ResBlock(resnet.res5),
-        x->mean(x,dims=(1,2)),
-        x -> reshape(x, :, size(x,4)),
+        x -> mean(x, dims = (1, 2)),
+        x -> reshape(x, :, size(x, 4)),
         ch2dense(resnet.fc6),
         softmax,
     )
 end
 
 
-r=myres()
-Flux.testmode!(r,true)
-ret=r(rand(Float32,224,224,3,1))
+r = myres()
+Flux.testmode!(r, true)
+ret = r(rand(Float32, 224, 224, 3, 1))
 size(ret)
