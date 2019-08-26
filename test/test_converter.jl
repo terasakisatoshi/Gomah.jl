8c5f96df10d519f6c84df81be11e062c472ef97a
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2019-08-26 14:51:43 +0900

    fix testcase

--- test/test_converter.jl
+++ test/test_converter.jl
@@ -1,6 +1,7 @@
 using Gomah
 using Gomah: L, np, @pywith
 using Flux
+using Statistics
 const DTYPE = Float32
 
 using Test
@@ -11,11 +12,12 @@ function test_ch2dense()
     OUTSIZE = 20
     BSIZE = 1
     chlinear = L.Linear(in_size = INSIZE, out_size = OUTSIZE)
-    dummyX = np.ones((BSIZE, INSIZE), dtype = np.float32)
-    chret = reversedims(chlinear(dummyX).array)
+    dummyX = 128*np.ones((BSIZE, INSIZE), dtype=np.float32)
+    dtype= chret = reversedims(chlinear(dummyX).array)
     fldense = ch2dense(chlinear)
     flret = fldense(reversedims(dummyX))
     @test size(flret) == size(chret)
+    @show err = mean(abs.(flret .- chret))
     @test all(isapprox.(flret, chret))
 end
 
@@ -30,18 +32,19 @@ function test_ch2conv()
     pad = 1
     s = 1
     chconv = Gomah.L.Convolution2D(
+        in_channels = INCH,
+        out_channels = OUTCH,
         ksize = KSIZE,
         pad = pad,
         stride = s,
-        in_channels = INCH,
-        out_channels = OUTCH
     )
     # pass dummy data to create computation graph
-    dummyX = np.ones((BSIZE, INCH, inH, inW), dtype = np.float32)
+    dummyX = np.ones((BSIZE, INCH, inH, inW),dtype=np.float32)
     chret = reversedims(chconv(dummyX).array)
     flconv = ch2conv(chconv)
     flret = flconv(ones(DTYPE, inW, inH, INCH, BSIZE))
     @test size(flret) == size(chret)
+    @show err = mean(abs.(flret .- chret))
     @test all(isapprox.(flret, chret))
 end
 
@@ -64,20 +67,21 @@ function test_ch2conv_nobias()
         nobias=true,
     )
     # pass dummy data to create computation graph
-    dummyX = np.ones((BSIZE, INCH, inH, inW), dtype = np.float32)
+    dummyX = 128*np.ones((BSIZE, INCH, inH, inW),dtype=np.float32)
     chret = reversedims(chconv(dummyX).array)
     flconv = ch2conv(chconv)
-    flret = flconv(ones(DTYPE, inW, inH, INCH, BSIZE))
+    flret = flconv(reversedims(dummyX))
     @test size(flret) == size(chret)
-    @test all(isapprox.(flret, chret))
+    @show maximum(abs.(flret .- chret))
+    @test all(isapprox.(abs.(flret-chret),0, atol=1e-4))
 end
 
 function test_resnetch2conv()
     # get instance of Convolution2D
-    INCH = 3
-    OUTCH = 8
-    inH = 30
-    inW = 30
+    INCH = 2
+    OUTCH = 3
+    inH = 7
+    inW = 7
     BSIZE = 1
     KSIZE = 7
     pad = 3
@@ -91,12 +95,13 @@ function test_resnetch2conv()
         nobias=true,
     )
     # pass dummy data to create computation graph
-    dummyX = np.ones((BSIZE, INCH, inH, inW), dtype = np.float32)
+    dummyX = 128*np.ones((BSIZE, INCH, inH, inW),dtype=np.float32)
     chret = reversedims(chconv(dummyX).array)
     flconv = ch2conv(chconv)
-    flret = flconv(ones(DTYPE, inW, inH, INCH, BSIZE))
+    flret = flconv(reversedims(dummyX))
     @test size(flret) == size(chret)
-    @test all(isapprox.(flret, chret))
+    @show maximum(abs.(flret .- chret))
+    @test all(isapprox.(abs.(flret-chret),0, atol=1e-4))
 end
 
 function test_ch2dwconv()
@@ -117,18 +122,19 @@ function test_ch2dwconv()
         stride = s
     )
     # pass dummy data to create computation graph
-    dummyX = np.ones((BSIZE, INCH, inH, inW), dtype = np.float32)
+    dummyX = 128*np.ones((BSIZE, INCH, inH, inW),dtype=np.float32)
     chret = reversedims(chdwconv(dummyX).array)
     fldwconv = ch2dwconv(chdwconv)
-    flret = fldwconv(ones(DTYPE, inW, inH, INCH, BSIZE))
+    flret = fldwconv(reversedims(dummyX))
     @test size(flret) == size(chret)
-    @test all(isapprox.(flret, chret))
+    @show maximum(abs.(flret .- chret))
+    @test all(isapprox.(abs.(flret-chret),0, atol=1e-4))
 end
 
 function test_ch2bn()
     SIZE = 10
     BSIZE = 1
-    dummyX = np.ones((BSIZE, SIZE))
+    dummyX = 128*np.ones((BSIZE, SIZE),dtype=np.float32)
     chbn = L.BatchNormalization(size = SIZE)
     @pywith chainer.using_config("train", false) begin
         chret = reversedims(chbn(dummyX).array)
@@ -136,7 +142,8 @@ function test_ch2bn()
         Flux.testmode!(flbn)
         flret = flbn(reversedims(dummyX))
         @test size(flret) == size(chret)
-        @test all(isapprox.(flret, chret))
+        @show maximum(abs.(flret .- chret))
+        @test all(isapprox.(abs.(flret-chret),0, atol=1e-4))
     end
 end
 
