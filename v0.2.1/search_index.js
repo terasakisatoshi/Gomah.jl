0d9e4630cbe55a8e10ffe850a53226888d0f8bd9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-27 18:33:23 +0000

    build based on ed78be6

---
+++ v0.2.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Gomah.jl-1","page":"Home","title":"Gomah.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [Gomah]","category":"page"}]
+}
